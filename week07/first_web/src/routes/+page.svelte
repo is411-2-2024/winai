99af2bee54ea39254c8016b210fca3f4908a38ae
Winai Nadee <winain@gmail.com>
2025-03-03 15:25:38 +0700

    Week 07

---
+++ week07/first_web/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Quantifier from "$lib/Quantifier.svelte";
+</script>
+
+<div>
+    <figure class="image is-128x128">
+        <img src="/logo-small.png" alt="mentor2code">
+    </figure>
+</div>
+<div>
+    <Quantifier /><hr>
+    <Quantifier /><hr>
+    <Quantifier /><hr>
+    <Quantifier /><hr>
+    <Quantifier /><hr>
+</div>
